3093e00a4b21716360626cb232ae5091789a9cbf
angelmfb <91120967+angelmfb@users.noreply.github.com>
2021-11-04 08:32:02 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>Gestion</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=0.5, maximum-scale=3.0"><!--Para que en el movil el ajuste lo haga bien-->
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+    <header>
+        <h1>Gestión de empleados</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="procesos/altaAdministrador.php">Alta</a></li>
+            <li><a href="procesos/listadoEmpleados.php">Listado</a></li>
+            <li><a href="procesos/consultaEmpleados.php">Consulta</a></li>
+        </ul>
+    </nav>
+    <div id="mover">
+        <nav>
+            <ul>
+                <li><a href="procesos/altaAdministrador.php">Alta</a></li>
+                <li><a href="procesos/listadoEmpleados.php">Listado</a></li>
+                <li><a href="procesos/consultaEmpleados.php">Consulta</a></li>
+            </ul>
+        </nav>
+        <main>
+            
+        </main>
+    </div>
+    <footer>
+        
+    </footer>
+</body>
+</html>
